2927e52595cfde29a003922d76c8ef9a9daad0df
Gianni Ridwan <99036552@mydavinci.nl>
2020-12-04 13:54:50 +0100

    Stappen 1 en 2 afgerond

---
+++ script.js
@@ -0,0 +1,62 @@
+var duck = document.getElementById("duck");
+var directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+var posLeft = 450;
+var posTop = 250;
+
+function fly(){
+	var number = Math.floor(Math.random() * directions.length);
+	var direction = directions[number];
+
+
+	if(direction == "N"){
+		posTop -= 50;
+		document.getElementById("duck").style.top = posTop + "px";
+	}
+
+	else if(direction == "NE"){
+		posTop -= 50;
+		posLeft += 50;
+		document.getElementById("duck").style.top = posTop + "px";
+		document.getElementById("duck").style.left = posLeft + "px";
+	}
+
+	else if(direction == "E"){
+		posLeft += 50;
+		document.getElementById("duck").style.left = posLeft + "px";
+	}
+
+	else if(direction == "SE"){
+		posLeft += 50;
+		posTop += 50;
+		document.getElementById("duck").style.left = posLeft + "px";
+		document.getElementById("duck").style.top = posTop + "px";
+	}
+
+	else if(direction == "S"){
+		posTop += 50;
+		document.getElementById("duck").style.top = posTop + "px";
+	}
+
+	else if(direction == "SW"){
+		posLeft -= 50;
+		posTop += 50;
+		document.getElementById("duck").style.left = posLeft + "px";
+		document.getElementById("duck").style.top = posTop + "px";
+	}
+
+	else if(direction == "W"){
+		posLeft -= 50;
+		document.getElementById("duck").style.left = posLeft + "px";
+	}
+
+	else{
+		posLeft += 50;
+		posTop -= 50;
+		document.getElementById("duck").style.left = posLeft + "px";
+		document.getElementById("duck").style.top = posTop + "px";
+	}
+}
+
+setInterval(function(){
+	fly();
+},500);
